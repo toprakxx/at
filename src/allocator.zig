81de868f9d6f4ca70b91f67485879087a415911f
toprakxx <179343804+toprakxx@users.noreply.github.com>
2026-02-16 04:20:37 +0300

    change allocator management stuff

--- src/allocator.zig
+++ src/allocator.zig
@@ -1,36 +1,24 @@
 const std = @import("std");
 
 pub const TA_Allocator = struct {
-    arena: ?std.heap.ArenaAllocator = null,
-    aa: ?std.mem.Allocator = null,
-
+    arena: []std.heap.ArenaAllocator = undefined,
+    gpa: std.heap.DebugAllocator = undefined, // FIXME
+    initialized: bool = false,
     pub fn new() TA_Allocator {
         return .{};
     }
-    pub fn init(self: *TA_Allocator) void {
-        if (self.arena == null) {
-            self.arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-            self.aa = self.arena.?.allocator();
+    pub fn init(self: *TA_Allocator, aa_num: u32) void {
+        if (!self.initialized) {
+            self.gpa = std.heap.GeneralPurposeAllocator(.{ .thread_safe = false }).init;
+            self.arena = self.gpa.allocator().alloc(std.heap.ArenaAllocator, aa_num) catch unreachable;
+            for (self.arena) |*a| {
+                a.* = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+            }
         }
     }
     pub fn deinit(self: *TA_Allocator) void {
-        if (self.arena) |a| {
+        for (self.arena) |*a| {
             a.deinit();
-            self.arena = null;
-            self.aa = null;
-        }
-    }
-    pub inline fn allocator(self: *TA_Allocator) std.mem.Allocator {
-        var buf: [512]u8 = undefined;
-        const slice = std.fmt.bufPrint(&buf, "dumb fuck \n go check {s}:{d}", .{
-            @src().file,
-            @src().line,
-        }) catch unreachable; // can never fail anyways cuz who the fuck has filename of ~500 chars
-        return self.aa orelse @panic(slice);
-    }
-    pub fn reset(self: *TA_Allocator, mode: std.heap.ArenaAllocator.ResetMode) void {
-        if (self.arena) |*a| {
-            _ = a.reset(mode);
         }
     }
 };
--- src/interface.zig
+++ src/interface.zig
@@ -9,19 +9,23 @@ pub const io = @import("io.zig");
 pub const TA_Allocator = @import("allocator.zig").TA_Allocator;
 const Keyboard = io.Keyboard;
 const KeyCode = io.KeyCode;
-var aAllocator: TA_Allocator = undefined;
+var allocator: TA_Allocator = undefined;
 var interface_kb: Keyboard = undefined;
-const entity = @import("entity.zig");
-const ecs = @import("ecs.zig");
+var canvas: Canvas = undefined;
 
 var initialized: bool = false;
 
 export fn interface_init() void {
     if (!initialized) {
-        aAllocator = TA_Allocator.new();
-        aAllocator.init();
+        allocator = TA_Allocator.new();
+        allocator.init(3); // create allocator with 3 arena and 1 gpa
         interface_kb = Keyboard.new();
         initialized = true;
+        canvas = Canvas.new(
+            800,
+            600,
+        );
+        canvas.init();
     }
 }
 // uhh no need to call since js sucks ass and you cant really call it also browser cleans up aftfer the program so
@@ -37,11 +41,11 @@ export fn keyboard_keyDown(key: u8) void {
 export fn keyboard_keyUp(key: u8) void {
     interface_kb.keyUp(@enumFromInt(key));
 }
-export fn canvas_getWidth() u32 {
-    return Canvas.width;
+export fn canvas_getWidth() i32 {
+    return canvas.width;
 }
-export fn canvas_getHeight() u32 {
-    return Canvas.height;
+export fn canvas_getHeight() i32 {
+    return canvas.height;
 }
 export fn canvas_getBuffer() [*]u32 {
     return canvas.getBuffer();
@@ -51,26 +55,23 @@ export fn canvas_clearBuffer() void {
 }
 // program entry
 var b: u8 = 128;
-var canvas: Canvas = Canvas.new();
 
 pub export fn update() void {
     if (interface_kb.iskeyDown(.w)) {
         b +%= 1;
-        io.print(aAllocator.allocator(), "New val for b: {d}", .{b});
-        aAllocator.reset(.retain_capacity);
+        io.print(allocator.gpa.allocator(), "New val for b: {d}", .{b});
     }
     if (interface_kb.iskeyDown(.s)) {
         b -%= 1;
-        io.print(aAllocator.allocator(), "New val for b: {d}", .{b});
-        aAllocator.reset(.retain_capacity);
+        io.print(allocator.gpa.allocator(), "New val for b: {d}", .{b});
     }
 
-    for (0..Canvas.height) |i| { // y
-        for (0..Canvas.width) |j| { // x
-            const r: u8 = @intCast((j * 255) / Canvas.width);
-            const g: u8 = @intCast((i * 255) / Canvas.height);
+    for (0..canvas.height) |i| { // y
+        for (0..canvas.width) |j| { // x
+            const r: u8 = @intCast((j * 255) / canvas.width);
+            const g: u8 = @intCast((i * 255) / canvas.height);
             const rgb = (@as(u24, r) << 16) | (@as(u24, g) << 8) | (@as(u24, b));
-            canvas.buffer[j + (i * Canvas.width)] = pixel.Pixel.fromRGB(rgb);
+            canvas.buffer[j + (i * canvas.width)] = pixel.Pixel.fromRGB(rgb);
         }
     }
     interface_kb.endFrame();
