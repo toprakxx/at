5e751d34920142ad063db6966c256ade123c8065
toprakxx <179343804+toprakxx@users.noreply.github.com>
2026-02-16 05:05:31 +0300

    switch gpa with wasm_allocator

--- src/allocator.zig
+++ src/allocator.zig
@@ -2,20 +2,26 @@ const std = @import("std");
 
 pub const TA_Allocator = struct {
     arena: []std.heap.ArenaAllocator = undefined,
-    gpa: std.heap.DebugAllocator = undefined, // FIXME
     initialized: bool = false,
+
     pub fn new() TA_Allocator {
         return .{};
     }
+
+    pub fn allocator(_: *TA_Allocator) std.mem.Allocator {
+        return std.heap.wasm_allocator;
+    }
+
     pub fn init(self: *TA_Allocator, aa_num: u32) void {
         if (!self.initialized) {
-            self.gpa = std.heap.GeneralPurposeAllocator(.{ .thread_safe = false }).init;
-            self.arena = self.gpa.allocator().alloc(std.heap.ArenaAllocator, aa_num) catch unreachable;
+            self.arena = std.heap.wasm_allocator.alloc(std.heap.ArenaAllocator, aa_num) catch unreachable;
             for (self.arena) |*a| {
-                a.* = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+                a.* = std.heap.ArenaAllocator.init(std.heap.wasm_allocator);
             }
+            self.initialized = true;
         }
     }
+
     pub fn deinit(self: *TA_Allocator) void {
         for (self.arena) |*a| {
             a.deinit();
